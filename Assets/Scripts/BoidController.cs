a17cfc23625827a244aa8db5228d97d03ad59fd3
chuc3 <chuc3@rpi.edu>
2014-10-06 04:49:17 -0400

    flocking

---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidController : MonoBehaviour
+{
+	public float minVelocity = 5;
+	public float maxVelocity = 20;
+	public float randomness = 1;
+	public int flockSize = 20;
+	public GameObject prefab;
+	public GameObject chasee;
+	
+	public Vector3 flockCenter;
+	public Vector3 flockVelocity;
+	
+	private GameObject[] boids;
+	
+	void Start()
+	{
+		boids = new GameObject[flockSize];
+		for (var i=0; i<flockSize; i++)
+		{
+			Vector3 position = new Vector3 (
+				Random.value * collider.bounds.size.x,
+				Random.value * collider.bounds.size.y,
+				0f//Random.value * collider.bounds.size.z
+				) - collider.bounds.extents;
+			Quaternion rot = new Quaternion(0f,0f,0f,0f);
+			GameObject boid = Instantiate(prefab, transform.position, rot) as GameObject;
+			boid.transform.parent = transform;
+			boid.transform.localPosition = position;
+			boid.GetComponent<BoidFlocking>().SetController (gameObject);
+			boids[i] = boid;
+			print (boid);
+		}
+	}
+	
+	void Update ()
+	{
+		Vector3 theCenter = Vector3.zero;
+		Vector3 theVelocity = Vector3.zero;
+		
+		foreach (GameObject boid in boids)
+		{
+			theCenter = theCenter + boid.transform.localPosition;
+			theVelocity = theVelocity + boid.rigidbody.velocity;
+			//keep z 0
+			Vector3 temp = boid.transform.position; 
+			temp.z = 2.0f; 
+			boid.transform.position = temp; 
+
+			// rotations stuff
+			Quaternion rotation = Quaternion.LookRotation
+				(boid.rigidbody.velocity, boid.transform.TransformDirection(Vector3.up));
+			boid.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+			/*
+			Quaternion tempQ = boid.transform.rotation; 
+			tempQ = Quaternion.identity;
+			boid.transform.rotation = tempQ;
+			if (boid.rigidbody.velocity != Vector3.zero) {
+				boid.transform.rotation = Quaternion.LookRotation((boid.rigidbody.velocity), Vector3.forward);
+				Quaternion tempqq = new Quaternion(0f,0f,boid.transform.rotation.z,1f);
+				boid.transform.rotation = tempqq;
+			}
+			*/
+			//transform.rotation=Quaternion.SetLookRotation(velocity);
+		}
+		
+		flockCenter = theCenter/(flockSize);
+		flockVelocity = theVelocity/(flockSize);
+	}
+}
---
+++ Assets/Scripts/FlockingAgent.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class FlockingAgent : MonoBehaviour {
+
+	// vars
+	public float speed;
+	public float visionAngle;
+	public float alignmentDistance;
+	public float cohesionDistance;
+	public float separationDistance;
+	
+	public float separationDistanceLarge;
+	public float separationWeightLarge;
+	
+	public float alignmentWeight;
+	public float cohesionWeight;
+	public float separationWeight;
+	
+	public float visionAngleLead;
+	public float alignmentDistanceLead;
+	public float cohesionDistanceLead;
+	public float separationDistanceLead;
+	
+	public float alignmentWeightLead;
+	public float cohesionWeightLead;
+	public float separationWeightLead;
+	
+	public float scatterDistance;
+	public float scatterSeparationWeight;
+
+
+	public List<GameObject> birds = new List<GameObject>();
+	public List<GameObject> leaders = new List<GameObject>();
+
+	Vector2 velocity;
+
+	// Use this for initialization
+	void Awake () {
+
+		rigidbody2D.velocity = transform.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angle = Vector2.Angle (Vector2.right, rigidbody2D.velocity);
+		if(Vector2.Angle (Vector2.up, rigidbody2D.velocity) > 90){
+			angle *= -1;
+		}
+		transform.rotation = Quaternion.Euler (new Vector3(0,0,angle));
+	}
+
+	void FixedUpdate(){
+		rigidbody2D.velocity = velocity;
+	}
+
+	public void Flock(){
+		Vector2 align = Align(sbirds, alignmentDistance, visionAngle);
+		Vector2 cohere = Cohere (birds, cohesionDistance);
+		Vector2 separate = Separate (birds, separationDistance);
+		
+		Vector2 sep2 = Separate(birds, separationDistanceLarge);
+		
+		Vector2 alignLead = Align(leaders, alignmentDistanceLead, visionAngleLead);
+		Vector2 cohereLead = Cohere (leaders, cohesionDistanceLead);
+		Vector2 separateLead = Separate (leaders, separationDistanceLead);
+		
+		Vector2 result = rigidbody2D.velocity +
+			(align * alignmentWeight) + (cohere * cohesionWeight) + (separate * separationWeight) + (sep2 * separationWeightLarge) +
+			(alignLead * alignmentWeightLead) + (cohereLead * cohesionWeightLead) + (separateLead * separationWeightLead);
+
+		velocity = result.normalized * speed;
+	}
+
+	Vector2 Align(List<GameObject> birds, float distance, float angle){
+		int count = 0;
+		return Align (birds, distance, angle, out count);
+	}
+
+	Vector2 Align(List<GameObject> birds, float distance, float angle, out int count){
+		Vector2 sum = Vector2.zero;
+		count = 0;
+		for(int i = 0; i < birds.Count; i++){
+			if(birds[i]!=null && birds[i] != gameObject && Vector2.Distance (birds[i].transform.position, transform.position) < distance){
+				if(Vector2.Angle (birds[i].transform.position - transform.position, transform.right) < angle){
+					sum += birds[i].rigidbody2D.velocity;
+					count++;
+				}
+			}
+		}
+		if(count == 0){
+			return sum;
+		}
+		sum.x = sum.x / count;
+		sum.y = sum.y / count;
+		return sum.normalized;
+	}
+
+	Vector2 Cohere(List<GameObject> birds, float distance){
+		int count = 0;
+		return Cohere (birds, distance, out count);
+	}
+	
+	Vector2 Cohere(List<GameObject> birds, float distance, out int count){
+		Vector2 sum = Vector2.zero;
+		count = 0;
+		for(int i = 0; i < birds.Count; i++){
+			if(birds[i]!=null && birds[i] != gameObject && Vector2.Distance (birds[i].transform.position, transform.position) < distance){
+				sum += (Vector2)birds[i].transform.position;
+				count++;
+			}
+		}
+		if(count == 0){
+			return sum;
+		}
+		sum.x = sum.x / count;
+		sum.y = sum.y / count;
+		sum = sum - (Vector2)transform.position;
+		return sum.normalized;
+	}
+
+	Vector2 Separate(List<GameObject> birds, float distance){
+		int count = 0;
+		return Separate (birds, distance, out count);
+	}
+	
+	Vector2 Separate(List<GameObject> birds, float distance, out int count){
+		Vector2 sum = Vector2.zero;
+		count = 0;
+		for(int i = 0; i < birds.Count; i++){
+			if(birds[i]!=null && birds[i] != gameObject && Vector2.Distance (birds[i].transform.position, transform.position) < distance){
+				sum += (Vector2)(birds[i].transform.position - transform.position);
+				count++;
+			}
+		}
+		if(count == 0){
+			return sum;
+		}
+		sum.x = sum.x / count;
+		sum.y = sum.y / count;
+		return -sum.normalized;
+	}
+}
