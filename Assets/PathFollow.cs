b576e87205e2646335c368b2aa2d11948dfc17fc
chuc3 <chuc3@rpi.edu>
2014-10-07 01:55:58 -0400

    start pt 2

---
+++ Assets/PathFollow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollow : MonoBehaviour {
+
+	public Transform pt1;
+	public Transform pt2;
+	public Transform pt3;
+	public Transform pt4;
+	public Transform pt5;
+	public Transform pt6;
+
+	int count;
+	float step;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		step = .01f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (count) 
+		{
+			case 0:
+				transform.position = Vector3.MoveTowards(transform.position, pt1.position, step);
+				break;	
+			case 1:
+				transform.position = Vector3.MoveTowards(transform.position, pt2.position, step);
+				break;	
+			case 2:
+				transform.position = Vector3.MoveTowards(transform.position, pt3.position, step);
+				break;
+			case 3:
+				transform.position = Vector3.MoveTowards(transform.position, pt4.position, step);
+				break;
+			case 4:
+				transform.position = Vector3.MoveTowards(transform.position, pt5.position, step);
+				break;
+			case 5:
+				transform.position = Vector3.MoveTowards(transform.position, pt6.position, step);
+				break;
+
+		}
+		if (transform.position == pt1.position
+			|| transform.position == pt2.position
+			|| transform.position == pt3.position
+			|| transform.position == pt4.position
+			|| transform.position == pt5.position)
+						count++;
+	}
+}
---
+++ Assets/coneCheck.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coneCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, fwd, 10f)) {
+			float angle = transform.rotation.w+10f;
+			//transform.rotation = Quaternion.Euler (new Vector3(0,0,angle));
+				}
+	}
+}
