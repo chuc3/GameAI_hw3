0286505ef6f2f6ea254665e8fb8bf01f753d2467
chuc3 <chuc3@rpi.edu>
2014-10-08 12:44:07 -0400

    part 2 mostly

--- Assets/coneCheck.cs
+++ Assets/coneCheck.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class coneCheck : MonoBehaviour {
 
+	RaycastHit hit;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,10 +12,13 @@ public class coneCheck : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 fwd = transform.TransformDirection(Vector3.forward);
-		RaycastHit hit;
-		if (Physics.Raycast (transform.position, fwd, 10f)) {
-			float angle = transform.rotation.w+10f;
-			//transform.rotation = Quaternion.Euler (new Vector3(0,0,angle));
+		Ray ray1 = new Ray (transform.position, transform.forward);
+		if (Physics.Raycast (ray1,out hit, 10f) && hit.transform.tag =="flocker") {
+			evade ();
 				}
+	}
+	void evade(){
+		print (hit.transform);
+		transform.position = Vector3.MoveTowards (transform.position, -hit.transform.position, 10f);
 	}
 }
